269b2989cd4c8622aac424b400533855ca9a4d33
江荣朝 <rcjiang@163.com>
2022-01-17 17:39:15 +0800

    update catalog api

--- mock/api/catalog.js
+++ mock/api/catalog.js
@@ -4,8 +4,30 @@ exports.a = {
   method: 'GET',
   handle (req) {
     const { id } = req.query
-    const data = hotRequire('./data/catalog.js')
-    const list = data[`data${id}`] || data.example
-    return list
+    const { data } = hotRequire('./data/catalog.js')
+    const flatData = flatTree(data)
+    const match = flatData.find(item => item.id === id)
+
+    return {
+      list: data,
+      match
+    }
   }
+}
+
+function flatTree (data, parent = []) {
+  if (!Array.isArray(data)) {
+    return []
+  }
+  return data.reduce((flatList, item) => {
+    const { parents = [], children, ...others } = item
+    flatList.push({
+      ...others,
+      parents: parent
+    })
+    if (children && children.length) {
+      flatList.push(flatTree(children), parent.concat(others.id))
+    }
+    return flatList
+  }, [])
 }
